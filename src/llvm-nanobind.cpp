acb69de6b9f705cfb99e95c3a8befc2b7472f4db
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2025-11-26 19:49:33 +0100

    LLVMContext

--- src/llvm-nanobind.cpp
+++ src/llvm-nanobind.cpp
@@ -2,11 +2,87 @@
 #include <nanobind/stl/string.h>
 #include <nanobind/stl/vector.h>
 
-int add(int a, int b) {
-    return a + b;
-}
+#include <llvm-c/Core.h>
+
+using namespace nanobind::literals;
+
+static void test() { auto llvm_context = LLVMContextCreate(); }
+
+struct LLVMContext {
+  LLVMContextRef m_ref = nullptr;
+  bool m_global = false;
+
+  explicit LLVMContext(bool global = false) : m_global(global) {
+    printf("LLVMContext(global: %d)\n", global);
+    if (global) {
+      m_ref = LLVMGetGlobalContext();
+    } else {
+      m_ref = LLVMContextCreate();
+    }
+  }
+
+  LLVMContext(LLVMContext &&other) noexcept : m_ref(other.m_ref) {
+    puts("LLVMContext(LLVMContext&&)");
+    other.m_ref = nullptr;
+  }
+
+  LLVMContext &operator=(LLVMContext &&other) noexcept {
+    puts("LLVMContext::operator=(LLVMContext&&)");
+    if (this != &other) {
+      LLVMContextDispose(m_ref);
+      m_ref = other.m_ref;
+      other.m_ref = nullptr;
+      other.m_global = false;
+    }
+    return *this;
+  }
+
+  ~LLVMContext() {
+    printf("~LLVMContext(%p)\n", m_ref);
+    if (m_ref != nullptr && !m_global) {
+      LLVMContextDispose(m_ref);
+    }
+  }
+
+  LLVMContext(const LLVMContext &) = delete;
+  LLVMContext &operator=(const LLVMContext &) = delete;
+};
+
+struct LLVMModule {
+  LLVMModuleRef m_ref = nullptr;
+
+  explicit LLVMModule(const std::string &name,
+                      const LLVMContext *context = nullptr) {
+    m_ref = LLVMModuleCreateWithNameInContext(
+        name.c_str(), context != nullptr ? context->m_ref : nullptr);
+  }
+
+  ~LLVMModule() {
+    printf("~LLVMModule(%p)\n", m_ref);
+    if (m_ref != nullptr) {
+      LLVMDisposeModule(m_ref);
+    }
+  }
+
+  LLVMModule(const LLVMModule &) = delete;
+  LLVMModule &operator=(const LLVMModule &) = delete;
+};
+static_assert(std::is_move_constructible_v<LLVMModule>);
 
 // Reference: https://nanobind.readthedocs.io/en/latest/basics.html
 NB_MODULE(llvm, m) {
-    m.def("add", &add);
+  nanobind::class_<LLVMContext>(m, "LLVMContext")
+      .def(nanobind::init<bool>(), "global_context"_a = false,
+           R"(Create a new LLVM Context.)")
+      .def_prop_rw(
+          "discard_value_names",
+          [](const LLVMContext &ctx) -> bool {
+            return LLVMContextShouldDiscardValueNames(ctx.m_ref);
+          },
+          [](LLVMContext &ctx, bool discard) {
+            LLVMContextSetDiscardValueNames(ctx.m_ref, discard);
+          },
+          R"(Return true if the Context runtime configuration is set to discard all value names.
+
+When true, only GlobalValue names will be available in the IR.)");
 }
--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 #!/opt/homebrew/bin/python3.14
 try:
-  import build.llvm as llvm
+  from build.llvm import LLVMContext
 except ModuleNotFoundError as e:
   raise ModuleNotFoundError(f"{e}\n  You can compile the native module with the following commands:\n    cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON\n    cmake --build build")
 
-print(llvm.add(1, 3))
+
+global_ctx = LLVMContext(global_context=True)
+
+ctx = LLVMContext()
+print(ctx.discard_value_names)
