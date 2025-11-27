eea50c83b0bb5e769dc7af58f3cd7b8f51d29c28
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2025-11-27 19:20:03 +0100

    Get context/module manager to work

--- src/llvm-nanobind.cpp
+++ src/llvm-nanobind.cpp
@@ -21,7 +21,7 @@ struct LLVMContext : NoMoveCopy {
   bool m_global = false;
 
   explicit LLVMContext(bool global = false) : m_global(global) {
-    printf("LLVMContext(global: %d)\n", global);
+    printf("%p:LLVMContext(global: %d)\n", this, global);
     if (global) {
       m_ref = LLVMGetGlobalContext();
     } else {
@@ -47,7 +47,7 @@ struct LLVMContext : NoMoveCopy {
   }
 
   ~LLVMContext() {
-    printf("~LLVMContext(%p)\n", m_ref);
+    printf("%p:~LLVMContext(%p)\n", this, m_ref);
     if (m_ref != nullptr && !m_global) {
       LLVMContextDispose(m_ref);
     }
@@ -59,13 +59,13 @@ struct LLVMModule : NoMoveCopy {
 
   explicit LLVMModule(const std::string &name,
                       const LLVMContext *context = nullptr) {
-    printf("LLVMModule(%s, %p)\n", name.c_str(), context);
+    printf("%p:LLVMModule(%s, %p)\n", this, name.c_str(), context);
     m_ref = LLVMModuleCreateWithNameInContext(
         name.c_str(), context != nullptr ? context->m_ref : nullptr);
   }
 
   ~LLVMModule() {
-    printf("~LLVMModule(%p)\n", m_ref);
+    printf("%p:~LLVMModule(%p)\n", this, m_ref);
     if (m_ref != nullptr) {
       LLVMDisposeModule(m_ref);
     }
@@ -79,11 +79,11 @@ struct LLVMException : std::runtime_error {
 struct LLVMContextManager : NoMoveCopy {
   std::unique_ptr<LLVMContext> m_context;
 
-  LLVMContext *enter() {
+  LLVMContext &enter() {
     if (m_context)
       throw LLVMException("LLVMContextManager already entered");
     m_context = std::make_unique<LLVMContext>();
-    return m_context.get();
+    return *m_context;
   }
 
   void exit(const nanobind::object &, const nanobind::object &,
@@ -105,23 +105,28 @@ struct LLVMModuleManager : NoMoveCopy {
   std::unique_ptr<LLVMModule> m_module;
 
   LLVMModuleManager(std::string name, LLVMContext *context = nullptr)
-      : m_name(std::move(name)), m_context(context) {}
+      : m_name(std::move(name)), m_context(context) {
+    printf("%p:LLVMModuleManager\n", this);
+  }
 
-  LLVMModule *enter() {
+  LLVMModule &enter() {
     if (m_module)
       throw LLVMException("LLVMModuleContext already entered");
     m_module = std::make_unique<LLVMModule>(m_name, m_context);
-    return m_module.get();
+    return *m_module;
   }
 
   void exit(const nanobind::object &, const nanobind::object &,
             const nanobind::object &) {
+    printf("%p:exit\n", this);
     if (!m_module)
       throw LLVMException("LLVMModuleContext not entered");
     m_module.reset();
   }
 
-  ~LLVMModuleManager() { printf("~LLVMModuleManager(%p)\n", m_module.get()); }
+  ~LLVMModuleManager() {
+    printf("%p:~LLVMModuleManager(%p)\n", this, m_module.get());
+  }
 };
 
 #if 0
@@ -138,8 +143,9 @@ nb::class_<PauseContext>(state, "PauseContext")
 state.def("pause", [](State &s) { return PauseContext{s}; }, nb::rv_policy::reference_internal);
 #endif
 
-static LLVMContext *global_context() {
-  return new LLVMContext(true);
+static LLVMContext &global_context() {
+  static LLVMContext context(true);
+  return context;
 }
 
 // Reference: https://nanobind.readthedocs.io/en/latest/basics.html
@@ -155,15 +161,30 @@ NB_MODULE(llvm, m) {
           },
           R"(Return true if the Context runtime configuration is set to discard all value names.
 
-When true, only GlobalValue names will be available in the IR.)");
+When true, only GlobalValue names will be available in the IR.)")
+      .def(
+          "create_module",
+          [](LLVMContext *self, const char *name) {
+            return new LLVMModuleManager(name, self);
+          },
+          R"(Create an LLVMModule in this context.)");
+
+  m.def("global_context", &global_context, R"(Get the global LLVM Context.)",
+        nanobind::rv_policy::reference);
 
   nanobind::class_<LLVMContextManager>(m, "LLVMContextManager")
-      .def(nanobind::init<>())
+      /*.def(nanobind::init<>(),
+           R"(Wrapper class for safely managing an LLVMContext.)")*/
       .def("__enter__", &LLVMContextManager::enter,
-           R"(Enter a new LLVM Context.)")
+           R"(Enter a new LLVM Context.)",
+           nanobind::rv_policy::reference_internal)
       .def("__exit__", &LLVMContextManager::exit,
            R"(Exit the current LLVM Context.)", "exc_type"_a.none(),
            "exc_value"_a.none(), "traceback"_a.none());
+
+  m.def(
+      "create_context", [] { return new LLVMContextManager(); },
+      R"(Create an LLVMModule.)");
 
   nanobind::class_<LLVMModule>(m, "LLVMModule")
       .def_prop_rw(
@@ -177,13 +198,11 @@ When true, only GlobalValue names will be available in the IR.)");
           R"(Get or set the data layout string for this module.)");
 
   nanobind::class_<LLVMModuleManager>(m, "LLVMModuleManager")
-      .def(nanobind::init<const std::string &, LLVMContext *>(), "name"_a,
-           "context"_a = nullptr, R"(Create a new LLVM Module Manager.)")
-      .def("__enter__", &LLVMModuleManager::enter,
-           R"(Enter a new LLVM Module.)")
-      .def("__exit__", &LLVMModuleManager::exit,
-           R"(Exit the current LLVM Module.)", "exc_type"_a.none(),
-           "exc_value"_a.none(), "traceback"_a.none());
-
-  m.def("global_context", &global_context, R"(Get the global LLVM Context.)");
+      /*.def(nanobind::init<const std::string &, LLVMContext *>(), "name"_a,
+           "context"_a = nullptr,
+           R"(Wrapper class for safely managing an LLVMModule.)")*/
+      .def("__enter__", &LLVMModuleManager::enter, R"(Create the module.)",
+           nanobind::rv_policy::reference_internal)
+      .def("__exit__", &LLVMModuleManager::exit, R"(Destroy the module.)",
+           "exc_type"_a.none(), "exc_value"_a.none(), "traceback"_a.none());
 }
--- test.py
+++ test.py
@@ -1,20 +1,13 @@
 #!/opt/homebrew/bin/python3.14
 try:
-  from build.llvm import LLVMContext, LLVMModule, LLVMContextManager, LLVMModuleManager, global_context
+  from build.llvm import global_context, create_context
 except ModuleNotFoundError as e:
   raise ModuleNotFoundError(f"{e}\n  You can compile the native module with the following commands:\n    cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON\n    cmake --build build")
 
 global_ctx = global_context()
-print("start context")
-with LLVMModuleManager("test_module") as module:
-  pass
-#with LLVMContextManager() as ctx:
-#  pass
-"""
-  print(ctx.discard_value_names)
-  print("begin module")
-  with LLVMModuleManager("test_module", ctx) as module:
+with global_ctx.create_module("test_module") as module:
+  print(module.data_layout)
+
+with create_context() as context:
+  with context.create_module("test_module") as module:
     print(module.data_layout)
-  print("end module")
-print("end context")
-"""
